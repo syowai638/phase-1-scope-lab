5538639a89d964b251fc7bd2d701baefa0d8d650
syowai638 <cmuthini77@gmail.com>
2024-12-05 00:40:57 +0300

    scope lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+// Declare a global variable called customerName
+var customerName = 'bob';
+
+// Function to uppercase the global customerName variable
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// Function to declare and assign a global variable bestCustomer
+function setBestCustomer() {
+  bestCustomer = 'not bob'; // Declaring without `var`, `let`, or `const` makes it global
+}
+
+// Function to overwrite the global bestCustomer variable
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+// Declare a constant in global scope called leastFavoriteCustomer
+const leastFavoriteCustomer = 'initial value';
+
+// Function to attempt changing the constant leastFavoriteCustomer
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'new value'; // This will throw an error
+}
